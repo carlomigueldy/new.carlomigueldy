210a34eb80dbb35de4c419c330f54426a59a809a
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-08-13 22:43:38 +0800

    It looks so cool now

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,34 @@
   <div>
     <nav>
       
-      <b-navbar toggleable="lg" type="light" variant="light" class="shadow-sm">
+      <b-navbar toggleable="lg" type="dark" class="shadow-sm fixed-top" style="background-color: #41b883">
         <div class="container">
-        <b-navbar-brand :to="{name: 'home'}">{Dy}</b-navbar-brand>
+        <b-navbar-brand :to="{name: 'home'}" class="my-nav-item" style="font-size: 30px">{ <span class="font-weight-bold">Dy</span> }</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           
           <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
+          <b-navbar-nav class="ml-auto text-uppercase font-weight-bold" style="font-size: 12px; color: white;">
             <b-navbar-nav>
-              <b-nav-item :to="{name: 'about'}">About</b-nav-item>
-              <b-nav-item :to="{name: 'contact'}">Contact</b-nav-item>
+              <b-nav-item :to="{name: 'home'}" class="my-nav-item">Home</b-nav-item>
+              <b-nav-item :to="{name: 'about'}" class="my-nav-item">About</b-nav-item>
+              <b-nav-item :to="{name: 'contact'}" class="my-nav-item">Contact</b-nav-item>
             </b-navbar-nav>
           </b-navbar-nav>
         </b-collapse>
         </div>
       </b-navbar>
     </nav>
-
-    <main class="container pt-3">
+  
+    <main class="my-bg-first" style="padding-top: 115px">
       <transition name="component-fade" mode="out-in">
         <router-view />
       </transition>
     </main>
   </div>
 </template>
-
-<style>
-body {
-  background-color: black;
-}
-.component-fade-enter-active, .component-fade-leave-active {
-  transition: opacity .3s ease;
-}
-.component-fade-enter, .component-fade-leave-to
-/* .component-fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
 
 <script>
 export default {
@@ -58,3 +46,53 @@ export default {
   }
 }
 </script>
+
+<style>
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.my-nav-item {
+  transition: all 0.5s;
+}
+.my-nav-item:hover {
+  transform: scale(1.2);
+}
+.my-color {
+  background-color: #DFDFDF;
+}
+.my-bg-first { 
+  background-color: #11273D;
+  color: white;
+  height: 100%; 
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.my-bg-second { 
+  background-color: #34495e;
+  height: 100%; 
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.my-bg-third { 
+  background-color: #8499AF;
+  height: 100%; 
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.my-card {
+  transition: all 1.5s;
+}
+.my-card:hover {
+  transform: scale(1.1)
+}
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,54 @@
 <template>
   <div>
-    <div class="badge badge-primary">
-      This is the home page
+
+    <div class="my-bg-first pb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="font-italic" style="font-size: 30px; padding-top: 100px">Hello there my name is, </div>
+            <div style="font-size: 60px">Carlo Miguel Dy</div>
+            <p>.. and I am a Laravel and Vue js Developer from the Philippines</p>
+          </div>
+          <div class="col text-center">
+            <img class="mt-5" src=".././assets/laravel-vue.svg" 
+            alt="Laravel & Vue" width="400px" height="400px">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="my-bg-second pt-5 pb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            ToolKit
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="my-bg-first pt-5">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
     </div>
   </div>
 </template>
 
 <style>
+img {
+  transition: all 1s;
+}
+img:hover {
+  transform: scale(1.05)
+}
 </style>
 
 <script>
