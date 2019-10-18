dc5f79b0d1471fe8215d45b50185a221873eb887
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-18 21:32:38 +0800

    cool VueTyper added

---
+++ src/components/About.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="hero is-light" id="About">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title has-text-centered">
+                    <i class="fas fa-user-circle"></i> About Me
+                </h1>
+                <section class="section">
+                    <div class="columns">
+                        <div class="column is-one-quarter">
+                            <center>
+                                <figure class="image is-128x128">
+                                    <img class="is-rounded" src="../assets/me.jpg">
+                                </figure>
+                            </center>
+                        </div>
+                        <div class="column">
+                            <p class="subtitle">
+                                I am a college undergraduate and in my last year in university with a Bachelor's Degree in Information Technology, specializing in Database Systems at <a target="_blank" href="http://msuiit.edu.ph" class="has-text-primary">Mindanao State University - Iligan Institute of Technology</a>. I'm a fullstack engineer, proficient in using HTML5, CSS, Javascript, PHP, MySQL, PostgreSQL, SQLite, utilizing frameworks like Laravel, Vue js and prior experience with Nuxt js and Ionic 4 on Web Development, also with experience in using OOP languages like Java, C++ and Python, and DBMS like MySQL, IBM DB2, PostgreSQL. I also have a background in Software Engineering, Project Management, Database Security, Administration and Management, Distributed Databases, Information Engineering, and Web Services.
+                            </p>
+                            <section class="section" id="Skills">
+
+                                <div class="title"><i class="fab fa-vuejs"></i> My Skills</div>
+                                <div class="content">
+                                    <ul class="columns">
+                                        <div class="column">
+                                            <li>HTML5</li>
+                                            <li>CSS</li>
+                                            <li>Javascript</li>
+                                            <li>PHP</li>
+                                            <li>Git</li>
+                                            <li>Laravel</li>
+                                            <li>Vue js</li>
+                                            <li>Vuex</li>
+                                        </div>
+                                        <div class="column">
+                                            <li>Vuetify js</li>
+                                            <li>Bulma</li>
+                                            <li>Buefy</li>
+                                            <li>Bootstrap 4</li>
+                                            <li>Leaflet js</li>
+                                            <li>C/C++</li>
+                                            <li>Java</li>
+                                            <li>Python</li>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+</template>
