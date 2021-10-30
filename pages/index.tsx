707044c9a32131936013c5c5fb3eeae323c47e25
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-10-30 13:52:50 +0800

    Update meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,21 @@ const Home: NextPage = () => {
         <meta name="title" content={META.title} />
         <meta name="description" content={META.description} />
         <link rel="icon" href="/favicon.svg" />
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        <meta name="application-name" content="carlomigueldy" />
+        <meta name="theme-color" content="#000000" />
         <meta name="keywords" content={META.keywords.join(", ")} />
+        <meta name="environment" content={META.environment} />
+        <link rel="canonical" href={META.url} />
 
         {/* Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content={META.url} />
         <meta property="og:title" content={META.title} />
+        <meta property="og:site_name" content={META.title} />
         <meta property="og:description" content={META.description} />
         <meta property="og:image" content={META.imageUrl} />
 
@@ -28,7 +36,14 @@ const Home: NextPage = () => {
         <meta property="twitter:description" content={META.description} />
         <meta property="twitter:url" content={META.url} />
         <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:creator" content="@CarloMiguelDy" />
         <meta property="twitter:image" content={META.imageUrl} />
+        <meta property="twitter:image:src" content={META.imageUrl} />
+
+        {/* Forem */}
+        <meta property="forem:name" content={META.title} />
+        <meta property="forem:logo" content={META.imageUrl} />
+        <meta property="forem:domain" content={META.url} />
       </Head>
 
       <AppHeader />
