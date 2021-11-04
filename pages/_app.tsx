c05150cfdc3b1e9b93e07298bd8e01542406f15d
carlomigueldy <carlomigueldy@gmail.com>
2021-11-04 12:54:00 +0800

    Add more assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,6 +47,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="title" content={META.title} />
         <meta name="description" content={META.description} />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
